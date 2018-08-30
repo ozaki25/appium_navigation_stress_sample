95818001d8fd906555d24ce792c489f4efa9d25d
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-30 23:57:29 +0900

    add router

--- App.js
+++ App.js
@@ -1,41 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 const styles = StyleSheet.create({
   container: {
@@ -44,14 +9,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+
+const TopPage = ({ navigation }) => (
+  <TouchableOpacity
+    style={styles.container}
+    onPress={() => {
+      navigation.navigate('TopPage');
+    }}
+  >
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+  </TouchableOpacity>
+);
+
+const Nav = StackNavigator({
+  TopPage: { screen: TopPage },
+});
+
+export default () => <Nav />;
